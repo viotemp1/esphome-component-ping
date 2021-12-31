3033e1eb3a8a78e69b1089ac88ed071cd2b81818
viotemp1 <viotemp1@icloud.com>
2021-12-31 17:26:08 +0200

    Update ping_esp32.h

--- components/ping/ping_esp32.h
+++ components/ping/ping_esp32.h
@@ -122,7 +122,7 @@ class PingSensorESP32 : public PingSensor {
     esp_ping_get_profile(hdl, ESP_PING_PROF_SEQNO, &seqno, sizeof(seqno));
     esp_ping_get_profile(hdl, ESP_PING_PROF_IPADDR, &target_addr, sizeof(target_addr));
     // this->set_latest_loss(100);
-    ESP_LOGW(TAG, "From %s icmp_seq=%d timeout", ipaddr_ntoa((ip_addr_t *) &target_addr), seqno);
+    // ESP_LOGW(TAG, "From %s icmp_seq=%d timeout", ipaddr_ntoa((ip_addr_t *) &target_addr), seqno);
   }
 
  protected:
